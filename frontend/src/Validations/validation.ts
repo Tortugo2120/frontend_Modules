2b7f47833166181073658933cadcc54fe99ec487
Michael Medina <micgaelma@gmail.com>
2026-01-30 12:47:50 -0500

    actualizacion del campo de username para las validaciones

--- frontend/src/Validations/validation.ts
+++ frontend/src/Validations/validation.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-    usuario: z.string()
+    username: z.string()
         .min(8, { message: "El usuario debe tener 8 dígitos" })
         .max(8, { message: "El usuario debe tener 8 dígitos" })
         .regex(/^\d+$/, { message: "El usuario debe contener solo números" }),
