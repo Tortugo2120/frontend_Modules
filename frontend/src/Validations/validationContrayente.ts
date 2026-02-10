43e850f900858866ac8aa3333778cb4052ea778c
Michael Medina <micgaelma@gmail.com>
2026-02-10 00:18:28 -0500

    cambios en la validacion del contrayente

--- frontend/src/Validations/validationContrayente.ts
+++ frontend/src/Validations/validationContrayente.ts
@@ -1,19 +1,17 @@
 import { z } from 'zod';
 
 export const contrayenteSchema = z.object({
-    dni: z
-        .string()
-        .min(1, 'El DNI es obligatorio')
-        .length(8, 'El DNI debe tener exactamente 8 dígitos')
-        .regex(/^\d{8}$/, 'El DNI debe contener solo números'),
-
+    documentTypeId: z.number().min(1).max(3),
+    cui: z
+        .string().min(1, 'El CUI es obligatorio')
+    ,
     names: z
         .string()
         .min(1, 'Los nombres son obligatorios')
         .min(2, 'Los nombres deben tener al menos 2 caracteres')
         .max(100, 'Los nombres no pueden exceder 100 caracteres')
-        .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Los nombres solo pueden contener letras y espacios'),
-
+        .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Los nombres solo pueden contener letras y espacios')
+    ,
     paternalSurname: z
         .string()
         .min(1, 'El apellido paterno es obligatorio')
@@ -67,6 +65,23 @@ export const contrayenteSchema = z.object({
             message: 'Debe seleccionar un estado civil válido'
         }),
 }).superRefine((values, ctx) => {
+    if(values.documentTypeId ===1){
+        if (!/^\d{8}$/.test(values.cui)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "El DNI debe tener 8 dígitos numéricos",
+            });
+        }
+    }
+    else{
+        if (values.cui.length < 5 || values.cui.length > 25) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "El documento debe tener entre 5 y 25 caracteres",
+            });
+        }
+    }
+
     const birthDate = new Date(values.birthdate);
     const today = new Date();
 
